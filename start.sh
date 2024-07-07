b1d96687b6f44641f1296e79b2ecb257492e7a4a
Oatech Inc <software@oatech.com.tw>
2024-07-07 16:57:40 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "WEB_USERNAME: $WEB_USERNAME"
+echo "WEB_PASSWORD: $WEB_PASSWORD"
+
 /filebrowser -r /pb -p 8080 --username=$WEB_USERNAME --password=$WEB_PASSWORD &
 
 /pb/pocketbase serve --http=0.0.0.0:443 &
