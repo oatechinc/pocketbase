1ed755bd5ccbd0e628a728a2c8ad59f04cb88ae5
Oatech Inc. <software@oatech.com.tw>
2024-08-21 16:35:11 +0800

    make port variable

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 echo "WEB_USERNAME: $WEB_USERNAME"
 echo "WEB_PASSWORD: $WEB_PASSWORD"
+echo "PORT: $PORT"
 
 /filebrowser config init
 /filebrowser users add $WEB_USERNAME $WEB_PASSWORD
@@ -9,7 +10,7 @@ echo "WEB_PASSWORD: $WEB_PASSWORD"
 
 /filebrowser -r /pb -p 8080 &
 
-/pb/pocketbase serve --http=0.0.0.0:443 &
+/pb/pocketbase serve --http=0.0.0.0:$WEB_PASSWORD &
 
 # 等待所有后台进程
 wait
