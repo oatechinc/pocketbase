458fce1fe720f4613fcd45e2d6766a19d75d4ea2
Ignacio Lee <ignacio@oatech.com.tw>
2024-08-21 16:51:24 +0800

    extract file to tmp and move to pb later

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN apk add --no-cache \
 
 # download and unzip PocketBase
 ADD https://github.com/pocketbase/pocketbase/releases/download/v${PB_VERSION}/pocketbase_${PB_VERSION}_linux_amd64.zip /tmp/pb.zip
-RUN unzip /tmp/pb.zip -d /pb/
+RUN unzip /tmp/pb.zip -d /tmp/pb
 
 EXPOSE 443
 EXPOSE 8080
--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# 僅當 /pb/pocketbase 不存在時才移動內容
+if [ ! -f "/pb/pocketbase" ]; then
+    mv /tmp/pb/* /pb/  # 移動 /tmp/pb 中的所有內容到 /pb
+    if [ $? -ne 0 ]; then
+        echo "Failed to move PocketBase contents" >&2
+        exit 1
+    fi
+fi
+
 echo "WEB_USERNAME: $WEB_USERNAME"
 echo "WEB_PASSWORD: $WEB_PASSWORD"
 echo "PORT: $PORT"
